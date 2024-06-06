8c85fad510d4c096fe6e347d5c82c1f5ed46de1d
Jackie-here <frostedivy42@gmail.com>
2024-06-06 11:17:35 +0800

    commented broken code

--- docs/bird_pictures.py
+++ docs/bird_pictures.py
@@ -4,10 +4,10 @@ import requests
 from bs4 import BeautifulSoup 
 
 
-response = requests.get()  
-html_content = response.content 
-soup = BeautifulSoup(html_content, 'html.parser') 
-image_urls = [img['src'] for img in soup.find_all('img')]
+#response = requests.get()  
+#html_content = response.content 
+#soup = BeautifulSoup(html_content, 'html.parser') 
+#image_urls = [img['src'] for img in soup.find_all('img')]
 
 'Great Egret'
 'Sparrow'
